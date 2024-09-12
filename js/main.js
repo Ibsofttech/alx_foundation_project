8b6434130f03251c6dfe7cd95ad3cfd6d82e821e
ibrahim muibi <ibrahimthesofttech@gmail.com>
2024-09-12 16:01:15 +0100

    progress

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Add smooth scroll for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetElement = document.querySelector(this.getAttribute('href'));
+
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
